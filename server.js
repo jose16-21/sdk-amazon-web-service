346a678116b5e15ce2dc51822d08ee82bf835551
Juan Hernandez <jhernandez@sistema.com.gt>
2018-06-15 17:02:03 -0600

    commit delete item

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const aws = require('aws-sdk');
 const app = express()
 
 aws.config.loadFromPath('./config.json');
-var dynamodb = new aws.DynamoDB();
+const dynamodb = new aws.DynamoDB();
+const unmarshalItem = require('dynamodb-marshaler').unmarshalItem;
 
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -13,8 +14,42 @@ app.listen(3000, function() {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+    var result = [];
+    var params = {
+        TableName: "Supervisors"
+    }
+    dynamodb.scan(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            res.render('index.ejs', { quotes: data.Items.map(unmarshalItem) });
+        }
+    });
+
+
 })
+
+app.get('/eliminar/:id', (req, res) => {
+    console.log(req.params.id);
+    var params = {
+        TableName: 'Supervisors',
+        Key: {
+            'name': { s: req.params.id },
+        }
+    };
+
+    // Call DynamoDB to delete the item from the table
+    dynamodb.deleteItem(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+            console.log("Success", data);
+        }
+    });
+    console.log('deleted');
+    res.redirect('/')
+});
+
 
 
 app.post('/quotes', (req, res) => {
